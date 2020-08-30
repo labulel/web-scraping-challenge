030b0607c50457e49a5d999520f7954f992ccaeb
L L <labulel@hotmail.com>
2020-08-30 19:03:25 -0400

    added

--- scrape_mars.py
+++ scrape_mars.py
@@ -18,7 +18,7 @@ def init_browser():
 def scrape():
     browser = init_browser()
 
-    # news_title, news_p = mars_news(browser)
+    news_title, news_paragraph = mars_news()
 
     # Run all scraping functions and store results in a dictionary
     data = {
